b49a8a68617859dc8dbabe1227dc24626b1b1890
tanakashun <michael021212@gmail.com>
2024-06-22 09:05:52 +0900

    views

---
+++ app/views/admin/keyword_mappings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "admin/keyword_mappings/keyword_mapping", keyword_mapping: @keyword_mapping
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :stores
     resources :machines
+    resources :keyword_mappings
     root to: 'homes#index'
   end
 
